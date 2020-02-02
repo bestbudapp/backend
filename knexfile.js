7bb3be1e7a1af2603af16a5ba53783a541a636f9
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-02 13:08:29 -0500

    express server and knexfile set up

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+	development: {
+		client: 'pg',
+		connection: {
+			database: process.env.DATABASE,
+			user: process.env.USER,
+			password: process.env.PASSWORD
+		},
+		migrations: {
+			directory: './data/seeds'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+	}
+};
