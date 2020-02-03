684eaf95e5d0a4029558cbbafc6188fff6b9b34b
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-03 15:50:56 -0500

    created authentication middleware

---
+++ auth/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (request, response, next) => {
+    const token = request.headers.authorization;
+    
+    if (token) {
+        jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+            if (err) {
+                response.status(500).json({message: 'error validating token'});
+            } else {
+                next();
+            };
+        });
+    } else {
+        response.status(401).json({message: 'unauthorized'})
+    };
+};
