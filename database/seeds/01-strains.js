a745d65351d0d8cc01e584a0b61dc94efdeb4f89
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-04 13:55:20 -0500

    minor changes

---
+++ database/seeds/01-strains.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const seeder = require('knex-seed-file');
+
+exports.seed = (knex, Promise) => {
+	return seeder(knex, './strains.csv', 'strains',
+		{
+			mapTo: ['id', 'name', 'flavors', 'race', 'positive_effects', 'negative_effects', 'medical_uses', 'rating', 'description'],
+			columnSeparator: '%',
+			rowSeparator: '\r',
+			ignoreFirstLine: true
+		}
+	);
+};
--- database/seeds/01-users.js
+++
@@ -1,8 +0,0 @@
-exports.seed = (knex, Promise) => {
-	return knex('users').insert([
-		{
-			username: 'jackBarry@test.com',
-			password: 'password'
-		}
-	]);
-};
