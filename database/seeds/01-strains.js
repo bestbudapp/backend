5abc830d18db6888f8267acb202080b88587afd0
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-04 13:56:42 -0500

    minor changes

--- database/seeds/01-strains.js
+++ database/seeds/01-strains.js
@@ -2,7 +2,7 @@ const path = require('path');
 const seeder = require('knex-seed-file');
 
 exports.seed = (knex, Promise) => {
-	return seeder(knex, './strains.csv', 'strains',
+	return seeder(knex, path.resolve('./strains.csv'), 'strains',
 		{
 			mapTo: ['id', 'name', 'flavors', 'race', 'positive_effects', 'negative_effects', 'medical_uses', 'rating', 'description'],
 			columnSeparator: '%',
