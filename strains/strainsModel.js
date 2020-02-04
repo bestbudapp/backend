f03398c3d185985537afaaa5c84a9e6e481165dd
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-04 12:06:01 -0500

    strain endpoints completed

--- strains/strainsModel.js
+++ strains/strainsModel.js
@@ -1,9 +1,19 @@
 const database = require('../database/config');
 
-const test = () => {
-    return database('strains');
+const fetch = (limit , offset) => {
+    return database('strains').limit(limit).offset(offset);
+};
+
+const fetchBy = (filter, query, limit, offset) => {
+    return database('strains').whereRaw(`LOWER(${filter}) LIKE ?`, [`%${query}%`]).limit(limit).offset(offset);
+};
+
+const fetchById = id => {
+    return database('strains').where({id}).first();
 };
 
 module.exports = {
-    test
+    fetch,
+    fetchBy,
+    fetchById
 };
--- strains/strainsRouter.js
+++ strains/strainsRouter.js
@@ -1,26 +1,73 @@
 const express = require('express');
-const axios = require('axios');
 const Strains = require('./strainsModel');
-// will not need a model because i will just be making calls to the data science api
 
 const app = express.Router();
 
-// fetch all strains OR fetch strains by a filter IF a body is passed
-app.get('/', (request, response) => {
-    if (request.body.strain !== undefined) {
-        response.send({message: 'strain specified, currently working with data science to set this endpoint up'});
-    } else {
-        response.send({message: 'currently working with data science to set this endpoint up'});
-    };
-});
+// fetch all strains with a limit and offset, returns an array
+app.get('/', validateFetch, (request, response) => {
+    const {limit, offset} = request.body;
 
-app.get('/test', (request, response) => {
-    Strains.test()
-        .then(res => {
-            response.status(200).json(res);
-            console.log(res[0].flavors.split(', '));
+    Strains.fetch(limit, offset)
+        .then(res => response.status(200).json(res))
+        .catch(err => {
+            response.status(500).json({message: 'error fetching strains'});
+            console.log(err);
         });
 });
-// need to talk to ds about pagination
+
+// fetch strains with a filter and a query, returns an array
+app.get('/query', validateFetchBy, (request, response) => {
+    const {filter, query, limit, offset} = request.body;
+
+    Strains.fetchBy(filter, query, limit, offset)
+        .then(res => response.status(200).json(res))
+        .catch(err => {
+            response.status(500).json({message: 'error fetching strains'});
+            console.log(err);
+        });
+});
+
+// fetch a single strain, returns an object
+app.get('/:id', (request, response) => {
+    const id = request.params.id;
+
+    Strains.fetchById(id)
+        .then(res => response.status(200).json(res))
+        .catch(err => {
+            response.status(500).json({message: 'error fetching strain'});
+            console.log(err);
+        });
+});
+
+// validation middleware
+
+function validateFetch(request, response, next) {
+    const {limit, offset} = request.body;
+
+    // will throw an error since false is equal to 0
+    if (!limit && limit !== 0) {
+        response.status(400).json({message: 'limit missing'});
+    } else if (!offset && offset !== 0) {
+        response.status(400).json({message: 'offset missing'});
+    } else if (typeof limit !== 'number') {
+        response.status(400).json({message: 'limit must be a number'});
+    } else if (typeof offset !== 'number') {
+        response.status(400).json({message: 'offset must be a number'});
+    } else {
+        next();
+    };
+};
+
+function validateFetchBy(request, response, next) {
+    const {filter, query, limit, offset} = request.body;
+
+    if (!filter || !query || !limit && limit !== 0 || !offset && offset !== 0) {
+        response.status(400).json({message: 'one or more fields missing'});
+    } else if (typeof filter !== 'string' || typeof query !== 'string' || typeof limit !== 'number' || typeof offset !== 'number') {
+        response.status(400).json({message: 'one or more fields are the wrong data type'});
+    } else {
+        next();
+    };
+};
 
 module.exports = app;
